e490a639282bb037f3f4e6c2acf37cbaaf46138e
Clint <cdunn6754@gmail.com>
2019-08-21 17:18:30 -0600

    Reorganize folders

--- app/main.cpp
+++ app/main.cpp
@@ -5,7 +5,7 @@
 #include "ReadCSV.h"
 
 int main() {
-    string data_str = "../app/data/ex1data2.txt";
+    string data_str = "../data/ex1data2.txt";
     Matrix mat = ReadCsv(data_str);
     cout << mat << endl;
     return 0;
---
+++ include/ReadCSV.h
@@ -0,0 +1,1 @@
+../src/ReadCSV/ReadCSV.h
