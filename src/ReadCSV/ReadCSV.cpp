552836f7ee073890200af6bbefe723229dfbc0a5
Clint <cdunn6754@gmail.com>
2019-08-21 18:05:03 -0600

    Minor work on reading csv

--- src/ReadCSV/ReadCSV.cpp
+++ src/ReadCSV/ReadCSV.cpp
@@ -16,24 +16,47 @@ using namespace boost::numeric::ublas;
 
 Matrix ReadCsv(string data_path) {
     ifstream csv_file;
-    Matrix m(47,3,0);
     string line;
        
     csv_file.open(data_path);
     if (!csv_file.is_open()) return Matrix(1,1,0);
 
-    int row_idx = 0;
+    std::vector<std::vector<double>> parsed_csv;
     while (getline(csv_file, line)) {
-	Tokenizer tok(line);
-	boost::numeric::ublas::vector<double> vec(3);
-	auto vec_it = vec.begin();
-	for (auto &s : tok){
-	    *vec_it = stod(s);
-	    vec_it++;
+	std::stringstream line_stream(line);
+	std::string element_string;
+	std::vector<double> parsed_row;
+	while(std::getline(line_stream, element_string, ',')) {
+	    parsed_row.push_back(stod(element_string));
 	}
-	row(m, row_idx) = vec;
-	row_idx++;  
+	parsed_csv.push_back(parsed_row);
     }
     csv_file.close();
-    return m;
+
+    if (parsed_csv.size() > 0 && parsed_csv[0].size() > 0) {
+	Matrix m(parsed_csv.size(),parsed_csv[0].size(),0);
+	for (auto &row : parsed_csv) {
+	    //matrix_row<Matrix> mr(m, 
+	}
+	return m;
+    }
+    else {
+	return Matrix(1,1,0);
+    }
+
+
+    
+    
+    // int row_idx = 0;
+    // while (getline(csv_file, line)) {
+    // 	Tokenizer tok(line);
+    // 	boost::numeric::ublas::vector<double> vec(3);
+    // 	auto vec_it = vec.begin();
+    // 	for (auto &s : tok) {
+    // 	    *vec_it = stod(s);
+    // 	    vec_it++;
+    // 	}
+    // 	row(m, row_idx) = vec;
+    // 	row_idx++;  
+    // }
 }
